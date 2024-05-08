686571848b1c833fbb06db5eb479d5d9ed4ce7ea
Andius76 <3182541950@qq.com>
2024-05-08 17:13:52 +0800

    first commit

---
+++ OpenCVdemo/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
+// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+public class Main {
+    public static void main(String[] args) {
+        System.load("C:\\Users\\ANDIUS\\Desktop\\AndiusIPS(Information Processing System).24.0.0\\opencv-4.8.0\\opencv\\build\\java\\x64\\opencv_java480.dll");
+
+    }
+}
